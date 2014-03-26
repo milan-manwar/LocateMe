15aa504188b422a08f079f7f1f8ddbf6d9b6e933
Milan Manwar <milan.manwar@gmail.com>
2014-03-26 11:27:19 +0530

    Initial Commit

---
+++ LocateMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LocateMe
+//
+//  Created by Omnipresent on 26/03/14.
+//  Copyright (c) 2014 Omnipresent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LocateMe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LocateMe
+//
+//  Created by Omnipresent on 26/03/14.
+//  Copyright (c) 2014 Omnipresent. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
